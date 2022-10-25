d43314f1c09bba7737af3df4d0ee5665e19f041d
dhapdhap123 <dhapdhap123@naver.com>
2022-10-25 23:49:32 +0900

    1025

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
+const { PORT } = process.env;
+
 const api = require('./api');
 
 const app = new Koa();
@@ -13,6 +15,7 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
