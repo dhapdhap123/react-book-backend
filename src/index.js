ff868458b2d9deaa5d5f7e455105fa629de7722d
dhapdhap123 <dhapdhap123@naver.com>
2022-10-26 17:58:31 +0900

    1026

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-
-const { PORT } = process.env;
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-router.use('/api', api.routes());
-
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
-
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
+/* eslint-disable no-global-assign */
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
